f58c8251bfb547bdb75e510fd0080dfc11f2c936
Wei Li <zeckli.devforgalaxy@gmail.com>
2020-02-15 01:18:36 +0800

    First try of gh-pages.

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  moduleFileExtensions: ['js', 'json', 'ts'],
-  testRegex: '(/__tests__/.*|(\\.|/)(test))\\.(ts|js)?$',
-  transform: {
-    '^.+\\.(ts)$': 'ts-jest'
-  },
-  coverageDirectory: './coverage/',
-  collectCoverage: true
-}
--- src/utils/index.ts
+++
@@ -1,5 +0,0 @@
-/**
- * An utility of generating an array.
- *
- */
-export const range = (size: number) => Array.from(Array(size).keys())
